10cd1ba021e75e7d7d7907d43843b6b28117707e
Diego <diegounifeibr@gmail.com>
2022-04-26 04:08:03 -0300

    add fish logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Logo from './images/liakad_piranha.png';
 import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-		<img alt="fish"/>
+		<img src={Logo} alt="fish"/>
 		<h1>Tank Size Calculator</h1>
-		<img alt="fish"/>
+		<img src={Logo} alt="fish"/>
       </header>
 
 	  <Calculator />
